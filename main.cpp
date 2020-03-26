ab36ff65d528709730c7e763beae26d362567497
Alice (NoskovaAV) <alice@example.com>
2020-03-26 16:49:22 +0300

    code: Max

--- main.cpp
+++ main.cpp
@@ -5,11 +5,22 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
-int a, b;
+int a, b, Max;
 cin >> a >> b;
+Max = a + b;
+    if (a-b>Max)
+        Max = a-b;
+
+    if (a*b>Max)
+        Max = a*b;
+
+    if (a/b>Max)
+        Max=a/b;
+
 cout << "A + B = " << a + b << '\n'
 << "A - B = " << a - b << '\n'
 << "A * B = " << a * b << '\n'
-<< "A / B = " << a / b << '\n';
+<< "A / B = " << a / b << '\n'
+<< "Max = " << Max << '\n'
     return 0;
 }
